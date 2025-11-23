f7beee23ea368473f326009e8f52e4c7b9230549
Benjamin <benjamink0612@gmail.com>
2025-11-23 13:44:31 +0200

    Made another change for secret key to not raise an error in git

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-not-for-production")
+SECRET_KEY = os.getenv("SECRET_KEY") or "dev-secret-key-not-for-production"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
